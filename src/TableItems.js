e5691f2ae40a2cb3c6d98bfc824f48c62b3c47cc
RdoEgito <33696073+RdoEgito@users.noreply.github.com>
2024-01-06 10:05:02 -0300

    Ajuste na rota para obtenção dos dados

--- src/TableItems.js
+++ src/TableItems.js
@@ -32,7 +32,7 @@ const TableItem = ({ onChangeQuantity }) => {
       };
     
     useEffect(() => {
-        fetch('api/items') // Rota definida no servidor para obter os itens
+        fetch('http://localhost:3001/api/items') // Rota definida no servidor para obter os itens
         .then(response => response.json())
         .then(data => {
             // Atualiza o estado com os dados obtidos do backend
