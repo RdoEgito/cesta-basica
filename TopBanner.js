7b1d79d3d176d7a6e743e27501590fe1d8c712e8
RdoEgito <rafael.rse@gmail.com>
2024-01-08 12:58:46 -0300

    Create TopBanner.js

---
+++ TopBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TopBanner = () => {
+  return (
+    <div className='top-banner'>
+      <img src='logo.png' alt='Logo IP Mandacaru' />
+      <div>
+        <p>Cesta Básica</p>
+        <p>IP Mandacaru</p>
+      </div>
+    </div>
+  );
+}
+
+expost default TopBanner;
