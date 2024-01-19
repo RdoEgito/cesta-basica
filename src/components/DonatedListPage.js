2942a2dd1c088707ae96b1dcc5e8ee2b87fe97bd
RdoEgito <rafael.rse@gmail.com>
2024-01-19 16:38:09 -0300

    Ajuste para validação

--- src/components/DonatedListPage.js
+++ src/components/DonatedListPage.js
@@ -6,31 +6,31 @@ import TopBanner from './TopBanner';
 const DonatedListPage = () => {
     // const [donatedItems, setDonatedItems] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                console.log("COMEÇANDO...");
-                // const responseDonatedItems = await axios.get('https://cesta-basica-api.vercel.app/api/donated-items');
-                // console.log(responseDonatedItems);
-                // const responseItems = await axios.get('https://cesta-basica-api.vercel.app/api/items');
-                // console.log(responseItems);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             console.log("COMEÇANDO...");
+    //             // const responseDonatedItems = await axios.get('https://cesta-basica-api.vercel.app/api/donated-items');
+    //             // console.log(responseDonatedItems);
+    //             // const responseItems = await axios.get('https://cesta-basica-api.vercel.app/api/items');
+    //             // console.log(responseItems);
 
-                // responseDonatedItems.data.map(donatedItem => {
-                //     const item = responseItems.filter(item => item.key == donatedItem.key)[0];
-                //     console.log(item);
-                //     setDonatedItems({
-                //         name: donatedItem.nome,
-                //         quantity: donatedItem.quantidade,
-                //         itemName: item.item
-                //     });
-                // });
-            } catch (error) {
-                console.error('Erro ao obter dados:', error);
-            }
-        };
+    //             // responseDonatedItems.data.map(donatedItem => {
+    //             //     const item = responseItems.filter(item => item.key == donatedItem.key)[0];
+    //             //     console.log(item);
+    //             //     setDonatedItems({
+    //             //         name: donatedItem.nome,
+    //             //         quantity: donatedItem.quantidade,
+    //             //         itemName: item.item
+    //             //     });
+    //             // });
+    //         } catch (error) {
+    //             console.error('Erro ao obter dados:', error);
+    //         }
+    //     };
 
-        fetchData();
-    }, []);
+    //     fetchData();
+    // }, []);
 
     return (
         <>
