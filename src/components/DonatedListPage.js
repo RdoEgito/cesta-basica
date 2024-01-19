72f74f72b29e9e2f5d45bf98d465c81f7a998488
RdoEgito <33696073+RdoEgito@users.noreply.github.com>
2024-01-19 19:08:34 -0300

    Remoção de log desnecessário

--- src/components/DonatedListPage.js
+++ src/components/DonatedListPage.js
@@ -10,22 +10,8 @@ const DonatedListPage = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                console.log("COMEÇANDO...");
                 const responseDonatedItems = await axios.get('https://cesta-basica-api.vercel.app/api/donated-items');
                 setDonatedItems(responseDonatedItems.data);
-                // console.log(responseDonatedItems);
-                // const responseItems = await axios.get('https://cesta-basica-api.vercel.app/api/items');
-                // console.log(responseItems);
-
-                // responseDonatedItems.data.map(donatedItem => {
-                //     const item = responseItems.filter(item => item.key == donatedItem.key)[0];
-                //     console.log(item);
-                //     setDonatedItems({
-                //         name: donatedItem.nome,
-                //         quantity: donatedItem.quantidade,
-                //         itemName: item.item
-                //     });
-                // });
             } catch (error) {
                 console.error('Erro ao obter dados:', error);
             }
