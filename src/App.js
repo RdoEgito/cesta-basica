17da7e0aab7250e63613377185575fd96ebbe6a1
RdoEgito <rafael.rse@gmail.com>
2024-01-19 14:49:27 -0300

    Ajuste nas referências às pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import DonationComponent from './pages/DonationComponent'
-import DonatedList from './pages/DonatedList'
+import DonationPage from './pages/DonationPage'
+import DonatedListPage from './pages/DonatedListPage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" exact element={<DonationComponent />} />
-      <Route path="/lista" element={<DonatedList />} />
+      <Route path="/" exact element={<DonationPage />} />
+      <Route path="/lista" element={<DonatedListPage />} />
     </Routes>
   </Router>
 );
