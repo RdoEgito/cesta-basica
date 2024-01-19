45dbe63ce8f2f75b3e4cf4bb4b7f5e8444150fcc
RdoEgito <rafael.rse@gmail.com>
2024-01-19 16:10:39 -0300

    Inclusão de lista de itens doados

--- src/components/DonatedListPage.js
+++ src/components/DonatedListPage.js
@@ -3,9 +3,55 @@ import React, { useState, useEffect } from 'react';
 import TopBanner from './TopBanner';
 
 const DonatedListPage = () => {
+    const [databaseItems, setDatabaseItems] = useState([]);
+    const [donatedItems, setDonatedItems] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const responseDonatedItems = await axios.get('https://cesta-basica-api.vercel.app/api/donated-items');
+                const responseItems = await axios.get('https://cesta-basica-api.vercel.app/api/items');
+
+                responseDonatedItems.data.map(donatedItem => {
+                    const item = responseItems.filter(item => item.key == donatedItem.key)[0];
+                    console.log(item);
+                    setDonatedItems({
+                        name: NamedNodeMap,
+                        quantity: quantidade,
+                        itemName: item.item
+                    });
+                });
+            } catch (error) {
+                console.error('Erro ao obter dados:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
             <TopBanner />
+            <div className='App'>
+            <table className="table">
+                {/* <thead>
+                <tr>
+                    <th>Item</th>
+                    <th className='col-quantity'>Qtd Necessária</th>
+                    <th className='col-quantity'>Qtd Doada</th>
+                </tr>
+                </thead> */}
+                <tbody>
+                {donatedItems.map((item, index) => (
+                    <tr key={index}>
+                        <td>{item.name}</td>
+                        <td>{item.quantity}</td>
+                        <td>{item.itemName}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            </div>
         </div>
     );
 };
