d272966e3531e61aa0752147fcd791b73c2c596d
RdoEgito <rafael.rse@gmail.com>
2024-01-19 16:51:24 -0300

    Ajustes na lista de doações

--- src/components/DonatedListPage.js
+++ src/components/DonatedListPage.js
@@ -4,47 +4,56 @@ import axios from 'axios';
 import TopBanner from './TopBanner';
 
 const DonatedListPage = () => {
-    // const [donatedItems, setDonatedItems] = useState([]);
+    const [donatedItems, setDonatedItems] = useState([]);
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             console.log("COMEÇANDO...");
-    //             // const responseDonatedItems = await axios.get('https://cesta-basica-api.vercel.app/api/donated-items');
-    //             // console.log(responseDonatedItems);
-    //             // const responseItems = await axios.get('https://cesta-basica-api.vercel.app/api/items');
-    //             // console.log(responseItems);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                console.log("COMEÇANDO...");
+                const responseDonatedItems = await axios.get('https://cesta-basica-api.vercel.app/api/donated-items');
+                console.log(responseDonatedItems);
+                const responseItems = await axios.get('https://cesta-basica-api.vercel.app/api/items');
+                console.log(responseItems);
 
-    //             // responseDonatedItems.data.map(donatedItem => {
-    //             //     const item = responseItems.filter(item => item.key == donatedItem.key)[0];
-    //             //     console.log(item);
-    //             //     setDonatedItems({
-    //             //         name: donatedItem.nome,
-    //             //         quantity: donatedItem.quantidade,
-    //             //         itemName: item.item
-    //             //     });
-    //             // });
-    //         } catch (error) {
-    //             console.error('Erro ao obter dados:', error);
-    //         }
-    //     };
+                responseDonatedItems.data.map(donatedItem => {
+                    const item = responseItems.filter(item => item.key == donatedItem.key)[0];
+                    console.log(item);
+                    setDonatedItems({
+                        name: donatedItem.nome,
+                        quantity: donatedItem.quantidade,
+                        itemName: item.item
+                    });
+                });
+            } catch (error) {
+                console.error('Erro ao obter dados:', error);
+            }
+        };
 
-    //     fetchData();
-    // }, []);
+        fetchData();
+    }, []);
 
     return (
         <>
             <TopBanner />
             <div>
-                {/* <table className="table">
-                    {donatedItems.map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.name}</td>
-                            <td>{item.quantity}</td>
-                            <td>{item.itemName}</td>
-                        </tr>
-                    ))}
-                </table> */}
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Quantidade</th>
+                        <th>Item</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {donatedItems.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.itemName}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </>
     );
