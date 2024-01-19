f4bd88c9757eb9f8d15bb4ff1374b40a29551acb
RdoEgito <rafael.rse@gmail.com>
2024-01-19 15:15:21 -0300

    Ajuste na estrutura de arquivos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import DonationPage from '/pages/DonationPage'
-import DonatedListPage from '/pages/DonatedListPage'
+import DonationPage from './components/DonationPage'
+import DonatedListPage from './components/DonatedListPage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => (
